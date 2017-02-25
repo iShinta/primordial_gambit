d0f2505a3ed89491a20636e289007d000d5f3294
codedeshi <singh.hardit@gmail.com>
2017-02-25 16:33:20 -0600

    moved all code to d_d app

--- apps/disaster_dispatch/views.py
+++ apps/disaster_dispatch/views.py
@@ -1,18 +1,40 @@
 import json
 import random
 from django.shortcuts import render, HttpResponse
-from ..ml.models import Incident, Call
-from ..ml import views as mlViews
+from .models import Incident, Call
+import imaplib
+import string
+from django.core import serializers
+
 
 # Create your views here.
 def index(request):
     return render(request, 'disaster_dispatch/index.html')
 
 def refresh(request):
-    raw_text = mlViews.read()
+    raw_text = read()
     print '*' * 100
     print raw_text
-    if len(raw_text) >= 0:
-        incident = Incident.objects.parse_text(raw_text)
-    all_incidents = Incident.objects.filter(is_resolved="False").order_by(priority)
-    return HttpResponse(json.dumps(response_data), content_type="application/json")
+    if len(raw_text) > 0:
+        for text in raw_text:
+            Incident.objects.parse_text(text.translate(None, string.punctuation))
+    all_incidents = Incident.objects.filter(is_resolved=False).order_by('priority')
+    all_incidents = serializers.serialize('json',all_incidents)
+    return HttpResponse(json.dumps(all_incidents), content_type="application/json")
+
+	
+def read():
+	imap = imaplib.IMAP4_SSL('imap.gmail.com')
+	imap.login('primordialgambit@gmail.com', 'md5hackathon')
+	imap.select('INBOX')
+	status, response = imap.search(None, '(UNSEEN)', '(FROM "Google Voice")')
+	unread_msg_nums = response[0].split()
+	da = []
+	if status == "OK":
+		for e_id in unread_msg_nums:
+			_, response = imap.fetch(e_id, '(RFC822)')
+			start = "<https://www.google.com/voice/>"
+			end = "play message"
+			res = response[0][1]
+			da.append(res[res.find(start)+len(start):res.find(end)].replace("\r\n",''))
+	return da
--- apps/ml/models.py
+++ apps/ml/models.py
@@ -11,22 +11,22 @@ class IncidentManager(models.Manager):
 		'flood': ['water', 'flooding']
 	}
 
-	def people_affected(raw_text):
+	def people_affected(self,raw_text):
 		raw_text = 'there are 100 to 150 people here'
 		p_index = raw_text.find('people')
 		people = 0
 		if p_index:
-			people = max([int(s) for s in raw_text[max(0,p_index-20):p_index].split() if s.isdigit()][0])
+			people = max([int(s) for s in raw_text[max(0,p_index-20):p_index].split() if s.isdigit()])
 		return people
 
-	def category(raw_text):
+	def category(self,raw_text):
 		for word in raw_text:
 			for category in kw:
 					if word in kw[category]:
 							return category
 		return 'unknown'
 
-	def location(raw_text):
+	def location(self,raw_text):
 		reg = re.compile(r"(?P<location>(.[0-9]+).{0,20}(lane|road|street))")
 		m =  re.search(reg,raw_text.lower())
 		m = m.groupdict()['location'].split()
@@ -38,7 +38,7 @@ class IncidentManager(models.Manager):
 		loc = [result['lng'],result['lat']]
 		return loc
 
-	def parse_text(raw_text):
+	def parse_text(self,raw_text):
 		data = {
 			'people_affected': self.people_affected(raw_text),
 			'category': self.category(raw_text),
@@ -46,7 +46,7 @@ class IncidentManager(models.Manager):
 		}
 		check_incident(data,raw_text)
 	
-	def haversine(lon1, lat1, lon2, lat2):
+	def haversine(self,lon1, lat1, lon2, lat2):
 		lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 		dlon = lon2 - lon1 
 		dlat = lat2 - lat1 
@@ -55,7 +55,7 @@ class IncidentManager(models.Manager):
 		km = 6367 * c
 		return km
 
-	def check_incident(data,raw_text):
+	def check_incident(self,data,raw_text):
 		try:
 			incidents = Incident.objects.filter(is_resolved=False)
 		except:
@@ -68,7 +68,7 @@ class IncidentManager(models.Manager):
 			return 'new incedent added'
 		
 		for incident in indicents:
-			distance = haversine(data['location'][0],data['location'][1],incident['location'][0],indicent['location'][1])
+			distance = self.haversine(data['location'][0],data['location'][1],incident['location'][0],indicent['location'][1])
 			if distance <= 1.6  and call['category'] == data['category']:
 				call = Call.objects.create(raw_text=raw_text,people=data['people_affected'],location=data['location'],indicent=incident)
 			else:
