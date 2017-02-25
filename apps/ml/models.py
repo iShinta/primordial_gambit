8527920ed5d5218a570d2efc198059890e935a55
Will Wright <wawwright@gmail.com>
2017-02-25 07:01:56 -0600

    added models

--- apps/ml/models.py
+++ apps/ml/models.py
@@ -3,3 +3,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Call(models.Model):
+    raw_text = models.TextField()
+    parsed_text = models.TextField()
+    location = models.CharField(max_length= 250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
+class Incident(models.Model):
+    priority = models.IntegerField()
+    is_resolved = models.BooleanField(initial=False)
+    category = models.CharField(max_length= 30)
+    location = models.CharField(max_length= 250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
