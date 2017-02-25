d60b24a447656e6ed3e7ec157164b3edfcd40e6d
Will Wright <wawwright@gmail.com>
2017-02-25 10:08:57 -0600

    cleaned up IncidentManager

--- apps/ml/models.py
+++ apps/ml/models.py
@@ -2,26 +2,39 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class ReviewManager(models.Manager):
+class IncidentManager(models.Manager):
 	kw = {
 			'fire' : ['fire', 'buring', 'burn'],
 			'shooting': ['shots fired', 'shooting', 'gun fire'],
 			'flood': ['water', 'flooding']
 	}
 
-def clean_data(data):
-	data = 'there are 100 to 150 people here'
-	p_index = data.find('people')
-	print p_index
-	if p_index:
-		people = max([int(s) for s in data[max(0,p_index-20):p_index].split() if s.isdigit()][0])
-	print people
+    def people_affected(raw_text):
+    	raw_text = 'there are 100 to 150 people here'
+    	p_index = raw_text.find('people')
+    	print p_index
+    	if p_index:
+    		people = max([int(s) for s in raw_text[max(0,p_index-20):p_index].split() if s.isdigit()][0])
+    	# print people
+        return people
 
-def category(keywords):
-	for word in keywords:
-		for i in kw:
-				if word in kw[i]:
-						return i
+    def category(raw_text):
+    	for word in raw_text:
+    		for category in kw:
+    				if word in kw[category]:
+    						return category
+
+    def location(raw_text):
+        reg = re.compile(r"(?P<location>(.[0-9]+).{0,20}(lane|road|street))")
+        m =  re.search(reg,string)
+        print m.groupdict()['location']
+
+    def parse_text(raw_text):
+        people_affected = self.people_affected(raw_text)
+        category = self.category(raw_text)
+        location = self.location(raw_text)
+        # do stuff not sure what to return
+
 
 
 # Create your models here.
@@ -32,6 +45,7 @@ class Incident(models.Model):
 	location = models.CharField(max_length= 250)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+    objects = IncidentManager()
 
 class Call(models.Model):
 	raw_text = models.TextField()
